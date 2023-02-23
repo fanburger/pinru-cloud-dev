02ad468f33a2dffacaabfea62cd76ca72aca41c5
YF <2922265540@qq.com>
2023-02-24 01:50:49 +0800

    🎉:Add some cloud file path

--- miniprogram/fileID.js
+++ miniprogram/fileID.js
@@ -1,6 +1,10 @@
 const bgLogo = 'cloud://could-dev-trail-3gt0m16oe3e39e91.636f-could-dev-trail-3gt0m16oe3e39e91-1316846436/miniprogram_images/login-bg.svg'
 const titleImageHome = 'cloud://could-dev-trail-3gt0m16oe3e39e91.636f-could-dev-trail-3gt0m16oe3e39e91-1316846436/titleImage/'
+const swiperImage = 'cloud://could-dev-trail-3gt0m16oe3e39e91.636f-could-dev-trail-3gt0m16oe3e39e91-1316846436/swiperImage/'
+const detailContent = 'cloud://could-dev-trail-3gt0m16oe3e39e91.636f-could-dev-trail-3gt0m16oe3e39e91-1316846436/detailContent/'
 export {
   bgLogo,
-  titleImageHome
+  titleImageHome,
+  swiperImage,
+  detailContent
 }
