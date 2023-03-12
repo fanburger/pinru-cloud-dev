f1ef62b7a5d4218be701039e9d54eb318956f885
fanburger <yinfanburger@gmail.com>
2023-03-12 23:47:30 +0800

    🎉: 新增地址选择组件

---
+++ miniprogram/components/address-selector/index.js
@@ -0,0 +1,32 @@
+// components/address-selector/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    userInfo: {}
+  },
+  lifetimes: {
+    attached: function () {
+      let userInfo = wx.getStorageSync('userInfo')
+      this.data.userInfo = userInfo
+      console.log(userInfo);
+      let {
+        _openid
+      } = userInfo
+    }
+  },
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
